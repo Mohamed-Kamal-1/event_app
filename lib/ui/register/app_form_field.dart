f6dd6de32ca97ba3f47e54de0321d72b945b1e30
moham <mk22443388@gmail.com>
2025-09-30 13:07:08 +0300

    feat: Create reusable AppFormField widget
    This commit introduces a new `AppFormField` widget.
    
    The `AppFormField` is a `StatefulWidget` that wraps a `TextFormField` and provides:
    - A label (`labelText`).
    - A prefix icon (`icon`).
    - Configurable keyboard type (`keyboardType`, defaults to `TextInputType.text`).
    - An optional suffix icon for password fields to toggle text visibility (`isPassword`, defaults to `false`).
    
    The text style uses `GoogleFonts.jockeyOne` with a font size of 18 and italic style.

---
+++ lib/ui/register/app_form_field.dart
@@ -0,0 +1,54 @@
+import 'package:evently_app/core/colors/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../l10n/app_localizations.dart';
+
+class AppFormField extends StatefulWidget {
+  String labelText;
+  IconData icon;
+  TextInputType keyboardType;
+  bool isPassword;
+
+  AppFormField({
+    required this.labelText,
+    required this.icon,
+    this.keyboardType = TextInputType.text,
+    this.isPassword = false,
+  });
+
+  @override
+  State<AppFormField> createState() => _AppFormFieldState();
+}
+
+class _AppFormFieldState extends State<AppFormField> {
+  bool isTextvisable = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: TextFormField(
+        obscureText: isTextvisable,
+        style: GoogleFonts.jockeyOne(fontSize: 18, fontStyle: FontStyle.italic),
+        keyboardType: widget.keyboardType,
+        decoration: InputDecoration(
+          labelText: widget.labelText,
+          prefixIcon: Icon(widget.icon),
+          suffixIcon: widget.isPassword
+              ? GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isTextvisable = !isTextvisable;
+                    });
+                  },
+                  child: Icon(
+                    isTextvisable ? Icons.visibility_off : Icons.visibility,
+                  ),
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
