5bf4ec163bfd9afe28624090d40417c0625ec3e2
moham <mk22443388@gmail.com>
2025-09-30 13:06:48 +0300

    feat: Add gray color to AppColor
    This commit adds a new color, `gray` (hex code `0xFF7B7B7B`), to the `AppColor` class.

--- lib/core/colors/app_color.dart
+++ lib/core/colors/app_color.dart
@@ -6,5 +6,6 @@ class AppColor{
   static const Color black = Color(0xFF1C1C1C);
   static const Color offWhite = Color(0xFFF4EBDC);
   static const Color darkBluePrimaryColor = Color(0xFF101127);
+  static const Color gray = Color(0xFF7B7B7B);
 
 }
