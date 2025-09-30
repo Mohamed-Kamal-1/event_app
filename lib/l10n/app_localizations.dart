1f2bedc95d5d51bd67c303a30eaceca41119beba
moham <mk22443388@gmail.com>
2025-09-30 13:08:10 +0300

    feat: Add localization strings for authentication and onboarding
    This commit introduces new localization strings to `AppLocalizations` for various elements related to user authentication and the onboarding process.
    
    New strings include:
    - `evently`
    - `commentOnBoardingScreenName`
    - `commentRegisterAndLoginScreenName`
    - `register`
    - `name`
    - `email`
    - `password`
    - `rePassword`
    - `createAccount`
    - `alreadyHaveAccount`
    - `login`
    - `forgetPassword`
    - `doNotHaveAccount`
    - `loginWithGoogle`
    - `resetPassword`

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -98,6 +98,18 @@ abstract class AppLocalizations {
     Locale('en'),
   ];
 
+  /// No description provided for @evently.
+  ///
+  /// In en, this message translates to:
+  /// **'Evently'**
+  String get evently;
+
+  /// No description provided for @commentOnBoardingScreenName.
+  ///
+  /// In en, this message translates to:
+  /// **'onBoardingScreen'**
+  String get commentOnBoardingScreenName;
+
   /// No description provided for @personalizeYourExperience.
   ///
   /// In en, this message translates to:
@@ -127,6 +139,84 @@ abstract class AppLocalizations {
   /// In en, this message translates to:
   /// **'Let’s Start'**
   String get letsStart;
+
+  /// No description provided for @commentRegisterAndLoginScreenName.
+  ///
+  /// In en, this message translates to:
+  /// **'Register_and_Login_'**
+  String get commentRegisterAndLoginScreenName;
+
+  /// No description provided for @register.
+  ///
+  /// In en, this message translates to:
+  /// **'Register'**
+  String get register;
+
+  /// No description provided for @name.
+  ///
+  /// In en, this message translates to:
+  /// **'Name'**
+  String get name;
+
+  /// No description provided for @email.
+  ///
+  /// In en, this message translates to:
+  /// **'Email'**
+  String get email;
+
+  /// No description provided for @password.
+  ///
+  /// In en, this message translates to:
+  /// **'Password'**
+  String get password;
+
+  /// No description provided for @rePassword.
+  ///
+  /// In en, this message translates to:
+  /// **'Re Password'**
+  String get rePassword;
+
+  /// No description provided for @createAccount.
+  ///
+  /// In en, this message translates to:
+  /// **'Create Account'**
+  String get createAccount;
+
+  /// No description provided for @alreadyHaveAccount.
+  ///
+  /// In en, this message translates to:
+  /// **'Already Have Account '**
+  String get alreadyHaveAccount;
+
+  /// No description provided for @login.
+  ///
+  /// In en, this message translates to:
+  /// **'Login '**
+  String get login;
+
+  /// No description provided for @forgetPassword.
+  ///
+  /// In en, this message translates to:
+  /// **'Forget Password'**
+  String get forgetPassword;
+
+  /// No description provided for @doNotHaveAccount.
+  ///
+  /// In en, this message translates to:
+  /// **'Don’t Have Account'**
+  String get doNotHaveAccount;
+
+  /// No description provided for @loginWithGoogle.
+  ///
+  /// In en, this message translates to:
+  /// **'Login With Google'**
+  String get loginWithGoogle;
+
+  /// No description provided for @resetPassword.
+  ///
+  /// In en, this message translates to:
+  /// **'Reset Password'**
+  String get resetPassword;
 }
 
 class _AppLocalizationsDelegate
